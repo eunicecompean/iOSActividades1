9c5bde21aaae570d446948cd335daf67261be325
Eunice Chavez <eunice.chavez>
2025-08-04 18:22:54 -0600

    Initial Commit

---
+++ HolaMundo2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HolaMundo2
+//
+//  Created by win603 on 04/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HolaMundo2/HolaMundo2App.swift
@@ -0,0 +1,17 @@
+//
+//  HolaMundo2App.swift
+//  HolaMundo2
+//
+//  Created by win603 on 04/08/25.
+//
+
+import SwiftUI
+
+@main
+struct HolaMundo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
